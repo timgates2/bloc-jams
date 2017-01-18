a9947a3a17fddf8f2a5ba018eb44fdc939210a86
timgates2 <timgates2@gmail.com>
2017-01-18 22:42:57 +0000

    checkpoint_27_Play/Pause_Part_2

--- scripts/album.js
+++ scripts/album.js
@@ -77,7 +77,21 @@ var albumPicasso = {
  };
  
  var getSongItem = function(element) {
-      
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    } 
  };
  var clickHandler = function(targetElement) {
       var songItem = getSongItem(targetElement);
